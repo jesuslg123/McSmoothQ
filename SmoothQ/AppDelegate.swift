33665eeb9237967e1859c35a8bd85883a8b823a8
Jesus Lopez <jesuslg123@gmail.com>
2018-12-23 20:14:49 +0100

    Initial Commit

---
+++ SmoothQ/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SmoothQ
+//
+//  Created by Jesus Lopez Garcia on 23/12/2018.
+//  Copyright © 2018 Jesus Lopez Garcia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
